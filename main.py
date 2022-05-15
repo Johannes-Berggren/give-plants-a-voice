7e18b3c6b826b7bd155c24eb102707602841cadb
Johannes Berggren <johannes@berggren.co>
2022-05-15 15:42:00 +0200

    Magnus' changes

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import time
 from gpiozero import MCP3008
-from gpiozero import RGBLED
+from gpiozero import LED
 import RPi.GPIO as GPIO
 import subprocess
 
+
 # Config
-moisture_threshold_lower = 600 # When to ask for water
-moisture_threshold_upper = 700 # When to say thank you
-escalation_delay = 60 * 30 # In seconds
+moisture_threshold_lower = 100 # When to ask for water
+moisture_threshold_upper = 450 # When to say thank you
+escalation_delay = 5 * 100 # In seconds
 
 # Pins
 moisture_sensor_pin = 5
 pir_pin = 21
-red_pin = 13
-green_pin = 19
-blue_pin = 26
+red_led = LED(16)
+green_led = LED(19)
+yellow_led = LED(13)
+blue_led = LED(12)
 
 moisture_sensor = MCP3008(channel=0, select_pin=moisture_sensor_pin)
 GPIO.setmode(GPIO.BCM)
@@ -22,7 +24,7 @@ GPIO.setup(pir_pin, GPIO.IN)
 disgruntlement = 0
 last_notification = 0
 
-led = RGBLED(red=red_pin, green=green_pin, blue=blue_pin)
+# led = RGBLED(red=red_pin, green=green_pin, blue=blue_pin)
 
 def moisture_below_lower_threshold():
     return moisture_sensor.value * 1023 <= moisture_threshold_lower 
@@ -37,17 +39,36 @@ def ask_for_water():
     print('Disgruntled!, level: ', disgruntlement)
 
     if disgruntlement == 1:
-        subprocess.call(["mpg123", "audio/hello-there.mp3"])
+        subprocess.call(["mpg123", "audio/Torst 1.mp3"])
 
     if disgruntlement == 2:
-        subprocess.call(["mpg123", "audio/awkward.mp3"])
+        subprocess.call(["mpg123", "audio/Torst 2.mp3"])
 
-    if disgruntlement >= 3:
-        subprocess.call(["mpg123", "audio/rude.mp3"])
+    if disgruntlement == 3:
+        subprocess.call(["mpg123", "audio/Torst 3.mp3"])
+        
+    if disgruntlement == 4:
+        subprocess.call(["mpg123", "audio/Torst 4.mp3"])
+        
+    if disgruntlement == 5:
+        subprocess.call(["mpg123", "audio/Torst 5.mp3"])
+        
+    if disgruntlement == 6:
+        subprocess.call(["mpg123", "audio/Torst 6.mp3"])
+        
+    if disgruntlement == 7:
+        subprocess.call(["mpg123", "audio/Torst 7.mp3"])
+        
+    if disgruntlement >= 8:
+        subprocess.call(["mpg123", "audio/Torst 6.mp3"])
 
 def say_thank_you():
-    subprocess.call(["mpg123", "audio/delicious.mp3"])
+    subprocess.call(["mpg123", "audio/Takk 1.mp3"])
     print('Thank you!')
+    
+def say_thank_you2():
+    subprocess.call(["mpg123", "audio/Takk 2.mp3"])
+    print('Thank you I love you!')
 
 while (1):
     print('Moisture: ', '{:.0f}'.format(moisture_sensor.value * 1023))
@@ -57,19 +78,36 @@ while (1):
     print('----------')
 
     if moisture_below_lower_threshold():
-        led.color = (1, 0, 0) # red
+        red_led.on() # red
+        green_led.off()
+        yellow_led.off()
     elif moisture_above_upper_threshold():
-        led.color = (0, 1, 0) # green
+        green_led.on() # green
+        red_led.off()
+        yellow_led.off()
     else:
-        led.color = (1, 0.2, 0) # yellow
+        yellow_led.on() # yellow
+        red_led.off()
+        green_led.off()
 
     if moisture_below_lower_threshold() and pir_active() and time.time() >= last_notification + escalation_delay:
         disgruntlement += 1
         ask_for_water()
         last_notification = time.time()
 
-    if disgruntlement > 0 and moisture_above_upper_threshold() and pir_active():
+    if disgruntlement < 4 and moisture_above_upper_threshold() and pir_active():
         say_thank_you()
         disgruntlement = 0
+        time.sleep(300)
+              
+    if disgruntlement >= 4 and moisture_above_upper_threshold() and pir_active():
+        say_thank_you2()
+        disgruntlement = 0
+        time.sleep(120)
+        
+    if pir_active():
+        blue_led.on()
+    else:
+        blue_led.off()
 
     time.sleep(1)
